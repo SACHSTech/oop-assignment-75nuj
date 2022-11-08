81190a76919a868de7138f7fe274914e7c4da639
shan <shan2005an@gmail.com>
2022-11-08 10:16:16 -0500

    need to commit more often

---
+++ src/Song.java
@@ -0,0 +1,21 @@
+/*
+import java.io.*;
+import java.util.*;
+
+public class Song {
+    private String songName;
+    private ArrayList<String> artists;
+
+    public Song(String songName, ArrayList<String> artists) {
+        this.songName = songName;
+        this.artists = artists;
+    }
+
+    public String getSongName() {
+        return songName;
+    }
+
+    public 
+}
+
+*/
---
+++ src/SpotifyList.java
@@ -0,0 +1,18 @@
+/*
+public abstract class SpotifyList {
+    private String listName;
+    private String creator;
+    private String listType;
+
+    public SpotifyList(String listName, String creator, String listType) {
+        this.listName = listName;
+        this.creator = creator;
+        this.listType = listType;
+        
+    }
+
+    public abstract int getNumSongs();
+
+}
+
+*/

8285efb1df59e0dd544d241fa5b23b590a3f324d
shan <shan2005an@gmail.com>
2022-11-08 10:11:41 -0500

    made abstract class

---
+++ src/CreatorList.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class CreatorList extends SpotifyList{
+    private ArrayList<Song> songsList;
+    private String artist;
+
+    public CreatorList(String listName, String creator, String listType) {
+        super(listName, creator, listType);
+    }
+
+    public void addSong(String songName, ArrayList <String> artists, int songOrder) {
+        songsList.add(songOrder, new Song(songName, artists));
+    }
+
+    public int getNumSongs() {
+        return songsList.size();
+    }
+}

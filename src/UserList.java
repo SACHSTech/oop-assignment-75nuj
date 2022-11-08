8de38fe85150892315b306423e0b5b5dabdb3723
shan <shan2005an@gmail.com>
2022-11-08 10:54:33 -0500

    create userlist class and getnumsongs method

---
+++ src/UserList.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class UserList extends SpotifyList{
+    private ArrayList<Song> songsList;
+    private String user;
+
+
+    public int getNumSongs() [
+        return songsList.size();
+    ]
+}
